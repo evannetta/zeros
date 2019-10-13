17ff257d42f51af3fd4efb3797219ddb2c55b888
Anna Evstigneeva <evannetta@gmail.com>
2019-10-13 12:26:34 +0300

    add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function zeros(expression) {
-  // your solution
+ let factorials = expression.split("*");
+  let zero = 0;
+  let twos = false;
+  let single = false;
+  for (let item of factorials) {
+    let step = item.split("!").length - 1;
+    let n = parseInt(item);
+    if (n < 5) {
+      continue;
+    }
+    //!!factorial
+    if (step === 2) {
+      if (Number.isInteger(n / 2)) {
+        zero += Math.trunc(n / 10);
+        if (n > 50) {
+          zero += (1 + Math.trunc(n / 100));
+        }
+        twos = true;
+      } else {
+        zero += Math.trunc((n + 5) / 10);
+        if (n > 25) {
+          zero++;
+        }
+      }
+      continue;
+    }
+    //!factorial
+    single = true;
+    let i = 1;
+    let add = n / Math.pow(5, i);
+    while (add >= 1) {
+      zero += Math.trunc(add);
+      i++;
+      add = n / Math.pow(5, i);
+    }
+  }
+  if (!single && !twos) {
+    return 0;
+  } else {
+    return zero;
+  }
 }
